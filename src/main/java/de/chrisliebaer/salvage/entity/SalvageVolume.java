44443dc219707353f3cedcb3590015bb7d59497c
Chrisliebär <11184804+chrisliebaer@users.noreply.github.com>
2023-04-27 16:12:45 +0200

    Allow crane pull image to fail if older version is available (#10)
    Sometimes pulling images fails due to service outages. When `pullOnRun` is used, an older image could still be available to use. If that is the case, there is no reason to refuse execution of tides. This change allows salvage to run tides even if it failed to pull the tides crane image. The tide itself will still be considered failed, since it goes against the user's intent to run old cranes when updates are explicitly required.

--- src/main/java/de/chrisliebaer/salvage/entity/SalvageVolume.java
+++ src/main/java/de/chrisliebaer/salvage/entity/SalvageVolume.java
@@ -4,8 +4,6 @@ import com.github.dockerjava.api.command.InspectVolumeResponse;
 
 public record SalvageVolume(String name, BackupMeta.VolumeMeta meta) {
 	
-	private static final String LABEL_VOLUME_TIDE_NAME = "salvage.tide";
-	
 	public static SalvageVolume fromInspectVolumeResponse(InspectVolumeResponse volume) {
 		var name = volume.getName();
 		var meta = BackupMeta.VolumeMeta.fromVolumeInspect(volume);
